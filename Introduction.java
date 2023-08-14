2b239e4f4e05297aecae62a50e5f833ad3c590b5
Gaurav Chaudhary <75659458+Gaurav-AL@users.noreply.github.com>
2023-08-14 13:21:26 +0530

    Update Introduction.java

--- Introduction.java
+++ Introduction.java
@@ -4,6 +4,6 @@ import java.io.*;
 public class Introduction{
   public static void main(String... args){
     System.out.println("Hello World");
-    System.out.println("Sum of two number "+(10+20) + " Done !");
+    System.out.println("Sum of the numbers "+(10+20+40) + " Done !");
   }
 }

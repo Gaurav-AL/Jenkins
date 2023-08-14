cac0756722d1b1d3e2f2cc8e1547bbd33cabb8f2
Gaurav Chaudhary <75659458+Gaurav-AL@users.noreply.github.com>
2023-08-14 12:38:26 +0530

    Update AddNumbers.java

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,6 +1,6 @@
 import java.io.*;
 public class AddNumbers{
-  public static void mian(String... agrs){
+  public static void main(String... agrs){
     int a  =10, b = 20;
     System.out.println("Sum of two numbers is : "+ (a+b));
   }

5c212e7bcfc2d4023c6ea6632732e3367209cd19
Gaurav Chaudhary <75659458+Gaurav-AL@users.noreply.github.com>
2023-08-14 12:48:17 +0530

    Update AddNumbers.java

--- AddNumbers.java
+++ AddNumbers.java
@@ -4,11 +4,6 @@ import java.util.Scanner;
 public class AddNumbers{
   public static void main(String... agrs){
     Scanner read = new Scanner(System.in);
-    int a , b ;
-    System.out.println("Enter First Number");
-    a = read.nextInt();
-    System.out.println("Enter Second Number");
-    b = read.nextInt();
-    System.out.println("Sum of two numbers is : "+ (a+b));
+    System.out.println("Sum of two numbers is : "+ (Integer.parseInt(args[0])+ Integer.parseInt(args[1])));
   }
 }
